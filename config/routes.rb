a27da2bae41acaed6cbccdf12c4e88efc18475fc
Zane Bookbinder <zanemb86@gmail.com>
2022-05-08 19:14:12 -0400

    made db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root "PPL-Final-Project-2022#index"
-  get "PPL-Final-Project-2022" => "PPL-Final-Project-2022#index"
-  get "/" => "PPL-Final-Project-2022#index"
-  post "/" => "PPL-Final-Project-2022#enterRoom"
+  resources :students
+  # root "PPLFinal-Project-2022#index"
+  # get "PPL-Final-Project-2022" => "PPL-Final-Project-2022#index"
+  # get "/" => "PPL-Final-Project-2022#index"
+  # post "/" => "PPL-Final-Project-2022#enterRoom"
 end
