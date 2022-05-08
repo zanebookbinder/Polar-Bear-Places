96dcd23eef31bb6dec621f874589e70cdd0c876c
Zane Bookbinder <zanemb86@gmail.com>
2022-05-08 19:24:53 -0400

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'polar_bear_places/index'
   resources :students
+  # get "/polarbearhousing" => 
   # root "PPLFinal-Project-2022#index"
   # get "PPL-Final-Project-2022" => "PPL-Final-Project-2022#index"
   # get "/" => "PPL-Final-Project-2022#index"
